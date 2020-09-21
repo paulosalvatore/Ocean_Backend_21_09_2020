63a027f70698b124d932733688acf2d6ff31bfc6
Paulo Salvatore <paulo@salvatore.tech>
2020-09-21 16:33:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', function (request, response) {
-    response.send('Hello world!');
+app.get('/', (req, res) => {
+    res.send('Hello world!');
 });
 
-// 
+// Endpoints de envio de mensagens
+// CRUD -> Create, Read (Read All e Read Single), Update and Delete
+// CRUD -> Criar, Ler (Ler tudo e ler individualmente), atualizar e remover
 
-app.post('/', function (request, response) {
-    response.send('POST Hello world!');
+// Read All
+app.get('/mensagens', (req, res) => {
+    res.send('Exibir toda a lista de mensagens.');
 });
 
-app.listen(port, function () {
+// Create
+app.post('/mensagens', (req, res) => {
+    res.send('Criar uma mensagem.');
+});
+
+// Read Single
+app.get('/mensagens/:id', (req, res) => {
+    res.send('Exibe uma mensagem selecionada pelo ID que foi informado.');
+});
+
+// Update
+app.put('/mensagens/:id', (req, res) => {
+    res.send('Atualiza uma mensagem selecionada pelo ID que foi informado.');
+});
+
+// Delete
+app.delete('/mensagens/:id', (req, res) => {
+    res.send('Remove uma mensagem selecionada pelo ID que foi informado.');
+});
+
+app.listen(port, () => {
     console.log(`App rodando em http://localhost:${port}`);
 });
