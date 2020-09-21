dc5f6370a87b804cbcd30fa2c196e24332a8ac96
Paulo Salvatore <paulo@salvatore.tech>
2020-09-21 17:06:35 -0300

    JSON parser, Read All e Create

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
+
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
@@ -10,14 +15,28 @@ app.get('/', (req, res) => {
 // CRUD -> Create, Read (Read All e Read Single), Update and Delete
 // CRUD -> Criar, Ler (Ler tudo e ler individualmente), atualizar e remover
 
+const mensagens = [
+    "Essa é uma mensagem",
+    "Essa é outra mensagem"
+];
+
 // Read All
 app.get('/mensagens', (req, res) => {
-    res.send('Exibir toda a lista de mensagens.');
+    res.json(mensagens);
 });
 
 // Create
 app.post('/mensagens', (req, res) => {
-    res.send('Criar uma mensagem.');
+    // Obtendo a mensagem que foi recebida através do body da requisição
+    const mensagem = req.body.mensagem;
+
+    // Insiro a mensagem na lista de mensagens
+    mensagens.push(mensagem);
+
+    const id = mensagens.length - 1;
+
+    // Exibido o ID da mensagem, que no caso é o índice que ela foi adicionada
+    res.send(`Mensagem criada. ID: ${id}.`);
 });
 
 // Read Single
