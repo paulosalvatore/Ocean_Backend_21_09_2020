0b0fb3e9f735922295df3c6ae1c0a1e34184575d
Paulo Salvatore <paulo@salvatore.tech>
2020-09-21 16:22:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', function (request, response) {
+    response.send('Hello world!');
+});
+
+// 
+
+app.post('/', function (request, response) {
+    response.send('POST Hello world!');
+});
+
+app.listen(port, function () {
+    console.log(`App rodando em http://localhost:${port}`);
+});
