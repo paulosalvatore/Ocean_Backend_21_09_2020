61187b78cdacaab0558b7560218fe978c90e69b4
Paulo Salvatore <paulo@salvatore.tech>
2020-09-21 16:22:56 -0300

    Port 3000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.get('/', function (request, response) {
     response.send('Hello world!');
